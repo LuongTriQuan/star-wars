46c654a9e06ec2d43392560fa12cdd94b546f6f3
LuongTriQuan <122350153+LuongTriQuan@users.noreply.github.com>
2023-02-23 18:32:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient
 
-const connectionString = ''
+const connectionString = 'mongodb+srv://root:2003@cluster0.xm0veoc.mongodb.net/?retryWrites=true&w=majority'
 
 // (0) CONNECT: server -> connect -> MongoDB Atlas 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
